903d72b3f53944053c3ad875d688e5e6aa91b6c7
Patrick Pedersen <patrick@ossia.net>
2016-11-21 10:13:40 +0100

    First version of php form

--- bestilling.php
+++ bestilling.php
@@ -3,16 +3,12 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
 
-$fornavn = $_REQUEST['fornavn'];
-$etternavn = $_REQUEST['etternavn'];
-$fdato = $_REQUEST['fdato'];
-$mobil = $_REQUEST['mobil'];
-$epost = $_REQUEST['epost'];
-$adresse = $_REQUEST['adresse'];
-$mtype = $_REQUEST['mtype'];
-$pris = $_REQUEST['pris'];
-
-$tekstfarge = "#67be2e";
+$navn = $_POST['navn'];
+$mobil = $_POST['mobil'];
+$epost = $_POST['epost'];
+$beskjed = $_POST['beskjed'];
+$antallpersoner = $_POST['antallpersoner']
+$tur = $_POST['tur']
 ?>
 <head>
 <link rel="stylesheet" media="(min-width: 601px)" href="css/stiler-vanlig.css">
@@ -25,14 +21,12 @@ $tekstfarge = "#67be2e";
   <?php
   echo "<p><b>Hei ".$fornavn."!</b><br><br>";
   echo "Du har oppgitt følgende for din innmelding i <b>O-klubben Rett Vest</b>:";
-  echo "<ul><li>Fornavn: <font color='".$tekstfarge."'>".$fornavn."</font></li>";
-  echo "<li>Etternavn: <font color='".$tekstfarge."'>".$etternavn."</font></li>";
-  echo "<li>Fødselsdato: <font color='".$tekstfarge."'>".$fdato."</font></li>";
-  echo "<li>Mobil: <font color='".$tekstfarge."'>".$mobil."</font></li>";
-  echo "<li>E-post: <font color='".$tekstfarge."'>".$epost."</font></li>";
-  echo "<li>Adresse: <font color='".$tekstfarge."'>".$adresse."</font></li>";
-  echo "<li>Medlemstype: <font color='".$tekstfarge."'>".$mtype."</font></li>";
-  echo "<li>Pris: <font color='".$tekstfarge."'>".$pris.",-/år</font></li></ul>";
+  echo "<ul><li>Fornavn:".$navn."</font></li>";
+  echo "<li>Mobil:".$mobil."</font></li>";
+  echo "<li>E-post:".$epost."</font></li>";
+  echo "<li>Beskjed:".$beskjed."</font></li>";
+  echo "<li>Tur:".$tur."</font></li>";
+  echo "<li>Antall Personer:".$antallpersoner."stk.</font></li></ul>";
   ?>
   </div>
 
